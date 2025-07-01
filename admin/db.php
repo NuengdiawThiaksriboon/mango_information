cd2e0f66cc2e3a7246fef8e83a8581d1d968396b
NaNo <nanoone342@gmail.com>
2025-07-01 14:28:55 +0700

    ทดสอบ

--- admin/db.php
+++ admin/db.php
@@ -9,9 +9,9 @@ $charset = 'utf8mb4';
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 $options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // แจ้ง error แบบ exception
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // ดึงข้อมูลแบบ array
-    PDO::ATTR_EMULATE_PREPARES   => false,                  // ใช้ prepared statement จริง
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
 ];
 
 try {
